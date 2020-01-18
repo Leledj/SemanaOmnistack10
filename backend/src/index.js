cc532c35b48b4488e4c1dacf69a4e898e2e445a6
Leandro Diniz <leandrodinizjesus.ld@gmail.com>
2020-01-18 17:41:08 -0300

    Aplicação desenvolvida com o Diego da Rocketseat

---
+++ backend/src/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const http = require('http');
+const routes = require('./routes');
+const { setupWebsocket } = require('./websocket');
+
+const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
+
+mongoose.connect('mongodb+srv://leledj12:Leledj12@cluster0-uuh3u.mongodb.net/week10?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+//Métodos HTTP: get, post, put, delete
+
+//Tipos de parâmetros:
+//Query Params: request.query (Filtros, ordenações, paginação, ...)
+//Route Params: request.params (Identificar um recurso na alteração ou remoção)
+//Body: request.body (Dados para criação ou alteração de um registro)
+
+server.listen(3333);
